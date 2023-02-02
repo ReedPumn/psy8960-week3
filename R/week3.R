7c7978d9cff6fe463a819c326cdcf6facd153e77
Reed W Priest <pries153@umn.edu>
2023-02-02 10:54:43 -0600

    Steps 1:9 done.

--- R/week3.R
+++ R/week3.R
@@ -3,3 +3,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
 raw_df <- read.csv(file = "../data/week3.csv", header = TRUE)
+raw_df$timeStart <- lapply(raw_df$timeStart, as.POSIXct)
+raw_df$timeEnd <- lapply(raw_df$timeEnd, as.POSIXct)
